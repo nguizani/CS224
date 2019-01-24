a99de90871457f1e301d82813d40a659f5b6366a
Nadra Guizani <guizani@gonzaga.edu>
2019-01-23 17:53:04 -0800

    again

---
+++ Assignment1/NamesDialog.java
@@ -0,0 +1,30 @@
+
+import javax.swing.JOptionPane;
+import java.lang.String;
+/**
+This program demonstrates using dialogs with 
+JOptionPane.
+**/
+
+public class NamesDialog
+{
+	public static void main(String[] args) 
+	{
+		String firstName; // The user's first name 
+		String middleName; // The user's middle name 
+		String lastName; // The user's last name
+		// Get the user's first name. 
+		firstName =
+		JOptionPane.showInputDialog("What is " + "your first name? ");
+		// Get the user's middle name. 
+		middleName =
+		JOptionPane.showInputDialog("What is " + "your middle name? ");
+		// Get the user's last name. 
+		lastName =
+		JOptionPane.showInputDialog("What is " + "your last name? ");
+		// Display a greeting 
+		JOptionPane.showMessageDialog(null, "Hello " + firstName + " " + middleName + " " + lastName);
+
+		System.exit(0); 
+	}
+}
