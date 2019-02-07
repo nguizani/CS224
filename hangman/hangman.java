6640c39f04e5f9252ddb0f8e5af74d23321ce7d7
Nadra Guizani <guizani@gonzaga.edu>
2019-02-07 08:29:22 -0800

    The beginning of the hangman game

--- hangman/hangman.java
+++ hangman/hangman.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class hangman{
 
-	String[] wordlist ={"Gonzaga", "BullDog","Boone", "Spring"};
+	String[] wordlist ={"Gonzaga", "Bull","Boone", "Spring"};
 	
 	public static void main(String[] args)
 	{
