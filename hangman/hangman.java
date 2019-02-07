e44afbfabdba2ca208fe9ad2d73658f54fdc020b
Nadra Guizani <guizani@gonzaga.edu>
2019-02-07 08:23:53 -0800

    The beginning of the hangman game

---
+++ hangman/hangman.java
@@ -0,0 +1,13 @@
+import java.lang.*
+import javax.swing.JOptionPane;
+import java.util.*;
+
+public class hangman{
+
+	String[] wordlist ={"Gonzaga", "BullDog","Boone", "Computer"};
+
+	public static void main(String[] args)
+	{
+		
+	}
+}
